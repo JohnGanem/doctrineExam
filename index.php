1518caeebbeb140916eed5c580157d9d50dc877a
JohnGanem <jonathan.ganem94@gmail.com>
2018-03-20 15:38:39 +0100

    commit

--- index.php
+++ index.php
@@ -7,43 +7,45 @@
     <body>
         <?php
         require_once './bootstrap.php';
-
-        $film = $entityManager->find('\Film', 3631);
-        $genre = $film->getGenre();
-        $distributeur = $film->getDistributeur();
-
-        $genre->getNom();
-        $distributeur->getNom();
-        //var_dump($film);
-
-        echo "<br><br>";
-
-        // liste film du genre Science fiction
-        $genre = $entityManager->find('\Genre', 2);
-        $filmG = $genre->getFilms();
-
-        echo $genre->getNom();
-        echo " comporte " . count($filmG) . " films";
-        echo "<br>";
-        // var_dump($filmG);
-
-        echo "<br><br>";
-
-        // liste film du distributeurs rezo Film
-        $distributeur = $entityManager->find('\Distributeur', 4);
-        $filmsD = $distributeur->getFilms();
-        echo $distributeur->getNom();
-        echo " comporte " . count($filmsD) . " films";
-        echo "<br>";
-        // var_dump($filmsD);
-
-        $fonction = $entityManager->find('\Fonction', 3);
-        $personne = $fonction->getPersonne();
-        echo "La fonction " . $fonction->getNom() . " est occupée par " . count($personne) . " employes";
-
-        $personne = $entityManager->find("\Personne", 145);
-        $fonctions = $personne->getFonction();
-        echo "<br>La personne " . $personne->getNom() . " occupe  " . count($fonctions) . " fonctions";
+        
+        
+//
+//        $film = $entityManager->find('\Film', 3631);
+//        $genre = $film->getGenre();
+//        $distributeur = $film->getDistributeur();
+//
+//        $genre->getNom();
+//        $distributeur->getNom();
+//        //var_dump($film);
+//
+//        echo "<br><br>";
+//
+//        // liste film du genre Science fiction
+//        $genre = $entityManager->find('\Genre', 2);
+//        $filmG = $genre->getFilms();
+//
+//        echo $genre->getNom();
+//        echo " comporte " . count($filmG) . " films";
+//        echo "<br>";
+//        // var_dump($filmG);
+//
+//        echo "<br><br>";
+//
+//        // liste film du distributeurs rezo Film
+//        $distributeur = $entityManager->find('\Distributeur', 4);
+//        $filmsD = $distributeur->getFilms();
+//        echo $distributeur->getNom();
+//        echo " comporte " . count($filmsD) . " films";
+//        echo "<br>";
+//        // var_dump($filmsD);
+//
+//        $fonction = $entityManager->find('\Fonction', 3);
+//        $personne = $fonction->getPersonne();
+//        echo "La fonction " . $fonction->getNom() . " est occupée par " . count($personne) . " employes";
+//
+//        $personne = $entityManager->find("\Personne", 145);
+//        $fonctions = $personne->getFonction();
+//        echo "<br>La personne " . $personne->getNom() . " occupe  " . count($fonctions) . " fonctions";
         ?>
     </body>
 </html>
